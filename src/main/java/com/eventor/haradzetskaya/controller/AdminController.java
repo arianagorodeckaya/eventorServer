63b666ba586503aa7a75b444bfc7984ce78d0c58
arianagorodeckaya <arianagorodeckaya.2@gmail.com>
2022-05-23 16:37:14 +0300

    Added method getUser() for Admin. Changed response in deleteUser() to ResponseEntity

--- src/main/java/com/eventor/haradzetskaya/controller/AdminController.java
+++ src/main/java/com/eventor/haradzetskaya/controller/AdminController.java
@@ -1,11 +1,16 @@
 package com.eventor.haradzetskaya.controller;
 
+import com.eventor.haradzetskaya.model.ErrorResponse;
 import com.eventor.haradzetskaya.model.Event;
 import com.eventor.haradzetskaya.model.User;
 import com.eventor.haradzetskaya.myEnum.Role;
+import com.eventor.haradzetskaya.service.EventService;
 import com.eventor.haradzetskaya.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,25 +23,39 @@ public class AdminController {
     @Autowired
     UserService userService;
     @Autowired
+    EventService eventService;
+    @Autowired
     private PasswordEncoder passwordEncoder;
 
 
     @GetMapping(path = "/users")
-    public  @ResponseBody
-    List<User> getAll() {
-        List<User> users = this.userService.getAll();
+    public Page<User> getAll(@RequestParam(defaultValue = "0") int page) {
+        Page<User> users = this.userService.getAll(page);
         for (User user: users) {
-            System.out.println("Before "+ user.getPw_hash());
-            user.setPw_hash(passwordEncoder.encode(user.getPw_hash()));
-            System.out.println("After "+ user.getPw_hash());
             user.setCreatorEvents(userService.setOnlyIdForUser(user));
+            for (Event event : user.getEvents()) {
+                event.setUsers(eventService.setOnlyIdForUsers(event));
+                event.setCreator(eventService.setOnlyIdForCreator(event.getCreator()));
+            }
         }
         return users;
     }
 
     @DeleteMapping(path = "/user")
-    void deleteUser(@RequestBody User user) {
+    ResponseEntity<?> deleteUser(@RequestBody User user) {
         userService.deleteUser(user.getId());
+        return ResponseEntity.ok(new ErrorResponse(HttpStatus.OK.value(), "User was deleted", System.currentTimeMillis()));
+    }
+
+    @GetMapping(path = "/user")
+    User getUser(@RequestBody User user) {
+        User outUser = userService.getById(user.getId());
+        outUser.setCreatorEvents(userService.setOnlyIdForUser(outUser));
+        for (Event event : outUser.getEvents()) {
+            event.setUsers(this.eventService.setOnlyIdForUsers(event));
+            event.setCreator(this.eventService.setOnlyIdForCreator(event.getCreator()));
+        }
+        return outUser;
     }
 
     @PostMapping(path = "/save")
