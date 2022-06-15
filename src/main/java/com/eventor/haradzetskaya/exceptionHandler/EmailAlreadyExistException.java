ab5e35823fa701d060b02d43d7ae5b03109d36ae
arianagorodeckaya <arianagorodeckaya.2@gmail.com>
2022-06-15 17:35:54 +0300

    Removed unnecessary constructors in EmailAlreadyExistException.class

--- src/main/java/com/eventor/haradzetskaya/exceptionHandler/EmailAlreadyExistException.java
+++ src/main/java/com/eventor/haradzetskaya/exceptionHandler/EmailAlreadyExistException.java
@@ -2,22 +2,9 @@ package com.eventor.haradzetskaya.exceptionHandler;
 
 
 public class EmailAlreadyExistException extends RuntimeException{
-    public EmailAlreadyExistException() {
-    }
 
     public EmailAlreadyExistException(String message) {
         super(message);
     }
 
-    public EmailAlreadyExistException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public EmailAlreadyExistException(Throwable cause) {
-        super(cause);
-    }
-
-    public EmailAlreadyExistException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
