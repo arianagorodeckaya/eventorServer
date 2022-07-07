3e78fa35f0aa99c70a6852a16ddaa99a88de674a
arianagorodeckaya <arianagorodeckaya.2@gmail.com>
2022-07-07 16:03:37 +0300

    1) Combined statistics methods to one with  @RequestParam String setting

--- src/main/java/com/eventor/haradzetskaya/controller/StatisticsAdminController.java
+++ src/main/java/com/eventor/haradzetskaya/controller/StatisticsAdminController.java
@@ -5,6 +5,7 @@ import com.eventor.haradzetskaya.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -18,36 +19,31 @@ public class StatisticsAdminController {
     @Autowired
     UserService userService;
 
-    @GetMapping(path = "/price")
-    public Map<String, Long> getStatisticsPrice() {
-        Map<String, Long> prices = new HashMap<>();
-        Long free = eventService.getCountFree();
-        Long paid = eventService.getCountPaid();
-        prices.put("Free", free);
-        prices.put("Paid", paid);
-        return prices;
+    @GetMapping
+    public Map<String, Long> getStatistics(@RequestParam String setting) {
+        Map<String, Long> statistics = new HashMap<>();
+        switch (setting) {
+            case "price":
+                Long free = eventService.getCountFree();
+                Long paid = eventService.getCountPaid();
+                statistics.put("Free", free);
+                statistics.put("Paid", paid);
+                break;
+            case "status":
+                Long countScheduled = eventService.getCountScheduled();
+                Long countEnded = eventService.getCountEnded();
+                Long countInProcess = eventService.getCountInProcess();
+                statistics.put("Scheduled", countScheduled);
+                statistics.put("In Process", countInProcess);
+                statistics.put("Ended", countEnded);
+                break;
+            case "entities":
+                Long users = userService.getCountUsers();
+                Long events = eventService.getCountEvents();
+                statistics.put("Users", users);
+                statistics.put("Events", events);
+                break;
+        }
+        return statistics;
     }
-
-    @GetMapping(path = "/status")
-    public Map<String, Long> getStatisticsStatus() {
-        Map<String, Long> status = new HashMap<>();
-        Long countScheduled = eventService.getCountScheduled();
-        Long countEnded = eventService.getCountEnded();
-        Long countInProcess = eventService.getCountInProcess();
-        status.put("Scheduled", countScheduled);
-        status.put("In Process", countInProcess);
-        status.put("Ended", countEnded);
-        return status;
-    }
-
-    @GetMapping(path = "/entities")
-    public Map<String, Long> getStatisticsEntities() {
-        Map<String, Long> entities = new HashMap<>();
-        Long users = userService.getCountUsers();
-        Long events = eventService.getCountEvents();
-        entities.put("Users", users);
-        entities.put("Events", events);
-        return entities;
-    }
-
 }
